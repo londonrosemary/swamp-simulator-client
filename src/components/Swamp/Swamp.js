d114a74b073430e0e64761632da8f33851a80349
London <london.rosemary@gmail.com>
2021-11-11 00:06:09 -0500

    Increase Pet Stats functionality added

--- src/components/Swamp/Swamp.js
+++ src/components/Swamp/Swamp.js
@@ -1,9 +1,126 @@
-import React from 'react';
+// parseInt((updatedPet.health + updatedPet.hunger + updatedPet.thirst + updatedPet.boredom) /4)
+import React, {useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom'
 
-function Swamp(){
+import "../Swamp/Swamp.styles.css"
+
+function Swamp({currentPet}){
+    const [updatedPet, setUpdatedPet] = useState({
+        health: currentPet.health,
+        hunger: currentPet.hunger,
+        thirst: currentPet.thirst,
+        boredom: currentPet.boredom
+    })
+    const [updatedHappiness, setUpdatedHappiness] = useState(currentPet.happiness)
+    
+    let navigate = useNavigate();
+    
+    function handleNavHome(){
+        navigate(-1)
+        navigate('home')
+    }
+
+    function increaseStat(stat){
+        console.log("current:", currentPet)
+        if (updatedPet[stat] >= 90) {
+            setUpdatedPet({...updatedPet, [stat]: 100})
+        }
+        else {
+            setUpdatedPet({...updatedPet, [stat]: updatedPet[stat] + 10})
+        }
+        setUpdatedHappiness(parseInt((updatedPet.health + updatedPet.hunger + updatedPet.thirst + updatedPet.boredom) /4))
+        console.log("updated:", updatedPet)
+        console.log(updatedHappiness)
+    }
+
+    // function increaseHealth(){
+    //     console.log("current:", currentPet)
+    //     if (updatedPet.health >= 90) {
+    //         setUpdatedPet({...updatedPet, health: 100})
+    //     }
+    //     else {
+    //         setUpdatedPet({...updatedPet, health: updatedPet.health + 10})
+    //     }
+    //     setUpdatedHappiness(parseInt((updatedPet.health + updatedPet.hunger + updatedPet.thirst + updatedPet.boredom) /4))
+    //     console.log("updated:", updatedPet)
+    //     console.log(updatedHappiness)
+    // }
+
+    // function increaseHunger(){
+    //     console.log("current:", currentPet)
+    //     if (updatedPet.hunger >= 90) {
+    //         setUpdatedPet({...updatedPet, hunger: 100})
+    //     }
+    //     else {
+    //         setUpdatedPet({...updatedPet, hunger: updatedPet.hunger + 10})
+    //     }
+    //     setUpdatedHappiness(parseInt((updatedPet.health + updatedPet.hunger + updatedPet.thirst + updatedPet.boredom) /4))
+    //     console.log("updated:", updatedPet)
+    //     console.log(updatedHappiness)
+    // }
+
+    // function increaseThirst(){
+    //     console.log("current:", currentPet)
+    //     if (updatedPet.thirst >= 90) {
+    //         setUpdatedPet({...updatedPet, thirst: 100})
+    //     }
+    //     else {
+    //         setUpdatedPet({...updatedPet, thirst: updatedPet.thirst + 10})
+    //     }
+    //     setUpdatedHappiness(parseInt((updatedPet.health + updatedPet.hunger + updatedPet.thirst + updatedPet.boredom) /4))
+    //     console.log("updated:", updatedPet)
+    //     console.log(updatedHappiness)
+    // }
+
+    // function increaseBoredom(){
+    //     console.log("current:", currentPet)
+    //     if (updatedPet.boredom >= 90) {
+    //         setUpdatedPet({...updatedPet, boredom: 100})
+    //     }
+    //     else {
+    //         setUpdatedPet({...updatedPet, boredom: updatedPet.boredom + 10})
+    //     }
+    //     setUpdatedHappiness(parseInt((updatedPet.health + updatedPet.hunger + updatedPet.thirst + updatedPet.boredom) /4))
+    //     console.log("updated:", updatedPet)
+    //     console.log(updatedHappiness)
+    // }
+    
+    useEffect(() => {
+        fetch(`https://swamp-simulator.herokuapp.com/pets/${currentPet.id}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                happiness: updatedHappiness,
+                health: updatedPet.health,
+                hunger: updatedPet.hunger,
+                thirst: updatedPet.thirst,
+                boredom: updatedPet.boredom
+            })
+        })
+            .then(resp => resp.json())
+            .then(data => console.log("POSTED:", data))
+    }, [updatedHappiness])
+        
     return(
-        <div>SWAMPY TEST</div>
+        <div>
+            <h1>Swamp</h1>
+            <button onClick={() => increaseStat("health")} >Give Medicine</button>
+            <button onClick={() => increaseStat("hunger")} >Give Food</button>
+            <button onClick={() => increaseStat("thirst")} >Give Water</button>
+            <button onClick={() => increaseStat("boredom")} >Give Toys</button>
+            <h3>Name: {currentPet.name}</h3>
+            <img className="swampImg" src={currentPet.image_url} />
+            <p>Overall Happiness: {updatedHappiness}%</p>
+            <p>Health: {updatedPet.health}%</p>
+            <p>Hunger: {updatedPet.hunger}%</p>
+            <p>Thirst: {updatedPet.thirst}%</p>
+            <p>Boredom: {updatedPet.boredom}%</p>
+            <button onClick={handleNavHome} >Back</button>
+        </div>
     )
 }
+
+
+
 
 export default Swamp;
