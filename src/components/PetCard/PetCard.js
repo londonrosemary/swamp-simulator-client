0b4a60f3db1fce4fb0104f59ef3ef201a3aa6c1a
London <london.rosemary@gmail.com>
2021-11-16 18:14:19 -0500

    Clean up code. Reverted to last functional version

--- src/components/PetCard/PetCard.js
+++ src/components/PetCard/PetCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {useNavigate} from 'react-router-dom';
 
 import "./PetCard.styles.css"
-function PetCard({pet, handleOnDelete}){
+function PetCard({pet, handleOnDelete, currentPet, setCurrentPet}){
     let navigate = useNavigate();
     
     function handleDelete(){
@@ -15,18 +15,16 @@ function PetCard({pet, handleOnDelete}){
     }
 
     function handleInteraction(){
+        setCurrentPet(pet)
         navigate('/swamp')
+        console.log("current pet:", currentPet)
     }
 
     return(
         <div>
-            <img src={pet.image_url} />
+            <img classname="cardImg" src={pet.image_url} />
             <h3>Name: {pet.name}</h3>
             <p>Overall Happiness: {pet.happiness}%</p>
-            <p>Health: {pet.health}%</p>
-            <p>Hunger: {pet.hunger}%</p>
-            <p>Thirst: {pet.thirst}%</p>
-            <p>Boredom: {pet.boredom}%</p>
             <button onClick={handleInteraction}>Interact</button>
             <button onClick={handleDelete} >Delete</button>
         </div>
